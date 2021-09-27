f2e6459fb54c627204a78a1cc45df276d728de0d
akash887 <62220909+akash887@users.noreply.github.com>
2021-09-28 02:14:53 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,11 +1,9 @@
 var random1 = Math.floor(Math.random() * 6) + 1;
-var randomeimage1 = "images/dice" + random1 + ".png";
-//var image =
-//alert(randomeimage1);
+var randomeimage1 = "./dice" + random1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", randomeimage1);
-// image.setAttribute("src", randomeimage1 );
+
 var random2 = Math.floor(Math.random() * 6) + 1;
-var randomeimage2 = "images/dice" + random2 + ".png";
+var randomeimage2 = "./dice" + random2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomeimage2);
 
 
